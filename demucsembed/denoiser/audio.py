177c30719bfc5d5c209bcfec07ea2dc349f342a7
balkce <caleb.rascon@gmail.com>
2024-02-29 12:54:22 -0600

    Fixed an issue with loading the complete audio with a new version of torchaudio.

--- demucsembed/denoiser/audio.py
+++ demucsembed/denoiser/audio.py
@@ -159,12 +159,11 @@ class Audioset:
         if self.length is not None:
             offset = self.stride * index
             num_frames = self.length
-        if torchaudio.get_audio_backend() in ['soundfile', 'sox_io']:
-            out, sr = torchaudio.load(str(audio_path),
-                                      frame_offset=offset,
-                                      num_frames=num_frames or -1)
-        else:
-            out, sr = torchaudio.load(str(audio_path), frame_offset=offset, num_frames=num_frames)
+        
+        out, sr = torchaudio.load(str(audio_path),
+                                  frame_offset=offset,
+                                  num_frames=num_frames or -1)
+        
         target_sr = self.sample_rate or sr
         target_channels = self.channels or out.shape[0]
         if self.convert:
